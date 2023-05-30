04b894c11d3cd0f386ce636dc02056e6330c4647
arafathsiam99 <md.arafathislamsiamnstuam14@gmail.com>
2023-05-30 13:02:30 +0600

    [DONE] setup express, mongoose, cors, parser

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from './config/index';
+
+async function tailwind() {
+    try {
+        await mongoose.connect(config.database_url as string);
+        console.log(`Database connected`);
+
+        app.listen(config.port, () => {
+            console.log(`Application listening on port ${config.port}`)
+        })
+
+    } catch (err) {
+        console.log(`Failed to connect`, err);
+    }
+}
+
+tailwind();
